5157f6c3de2a16b3478a82738b2ce8ba00520228
Emre ÖZAKYILDIZ <48735717+emreozakyildiz@users.noreply.github.com>
2023-05-22 06:59:09 +0300

    Environments Created.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,6 +4,23 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ProductService {
+  private apiUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(`${this.apiUrl}/all`);
+  }
+
+  public addBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(`${this.apiUrl}/add`, book);
+  }
+
+  public updateBook(book: Book): Observable<Book> {
+    return this.http.put<Book>(`${this.apiUrl}/update`, book);
+  }
+
+  public deleteBook(bookISBN: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}delete/${bookISBN}`);
+  }
 }
