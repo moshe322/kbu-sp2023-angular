c77ba898a3c35acf9fd928a323faaa4f11148751
Emre ÖZAKYILDIZ <48735717+emreozakyildiz@users.noreply.github.com>
2023-05-25 01:54:15 +0300

    First Draft Completed.

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-
+  @Input() product: Product | undefined;
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -3,6 +3,9 @@ import { Product } from '../product';
 import { Observable, OperatorFunction, debounceTime, distinctUntilChanged, map } from 'rxjs';
 import { ProductService } from '../product.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { NgbTypeaheadSelectItemEvent } from '@ng-bootstrap/ng-bootstrap';
+import { RecommendationService } from '../recommendation.service';
+import { Recommendations } from '../recommendations';
 
 @Component({
   selector: 'app-search',
@@ -15,6 +18,10 @@ export class SearchComponent {
 
   public model: any;
   public products: Product[] = [];
+  public selectedProduct: Product | undefined;
+  public recommendations! : Recommendations;
+  public recommendedProducts : Product[] = [];
+
   public search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
     text$.pipe(
       debounceTime(200),
@@ -26,7 +33,7 @@ export class SearchComponent {
       )
     );
 
-  constructor(private productService: ProductService){}
+  constructor(private recommendationService: RecommendationService, private productService: ProductService){}
   ngOnInit(): void {
     this.getProducts();
   }
@@ -41,4 +48,19 @@ export class SearchComponent {
       }
     );
   }
+
+  public onSelect(event: NgbTypeaheadSelectItemEvent): void {
+    this.selectedProduct = this.products[this.products.findIndex(product => product.productName === event.item)]
+    this.recommendationService.getRecommendations(this.selectedProduct.productID.toString())
+      .subscribe(
+        (data: Recommendations) => {
+          this.recommendations = data;
+          this.recommendedProducts = this.products.filter(product => this.recommendations.item_ids.includes(Number(product.productID)))
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  }
+  
 }
