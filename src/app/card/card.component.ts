0619fbab1e1ae3586d5a4ef851f160e38291274a
Emre ÖZAKYILDIZ <48735717+emreozakyildiz@users.noreply.github.com>
2023-05-26 15:55:30 +0300

    TRY Currency symbol is being added.

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../product';
+import { getCurrencySymbol } from '@angular/common';
 
 @Component({
   selector: 'app-card',
@@ -8,6 +9,7 @@ import { Product } from '../product';
 })
 export class CardComponent {
   @Input() product: Product | undefined;
+  myCurrency = getCurrencySymbol("TRY", "narrow");
 
   getMarketLogoUrl(marketID: number): string {
     return `assets/images/markets/market-${marketID}.png`;
