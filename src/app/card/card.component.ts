c2fe8e3b6137e35dc07187383e77d361b7aeb4ad
Emre ÖZAKYILDIZ <48735717+emreozakyildiz@users.noreply.github.com>
2023-06-26 09:21:05 +0300

    Chart completed.

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../product';
 import { getCurrencySymbol } from '@angular/common';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ChartComponent } from '../chart/chart.component';
 
 @Component({
   selector: 'app-card',
@@ -11,7 +13,15 @@ export class CardComponent {
   @Input() product: Product | undefined;
   myCurrency = getCurrencySymbol("TRY", "narrow");
 
+  constructor(private modalService: NgbModal) {}
+
   getMarketLogoUrl(marketID: number): string {
     return `assets/images/markets/market-${marketID}.png`;
+  }
+
+  getChart(priceData: number[]) {
+    const modalRef = this.modalService.open(ChartComponent);
+    modalRef.componentInstance.priceData = priceData;
+    console.log(priceData)
   }
 }
--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent {
+export class ChartComponent implements AfterViewInit {
+  @ViewChild('chartCanvas') chartCanvas?: ElementRef;
 
+  @Input() priceData?: number[];
+
+  constructor(private activeModal: NgbActiveModal) {}
+
+  ngAfterViewInit() {
+    Chart.register(...registerables);
+
+    const ctx = document.getElementById('price-chart') as HTMLCanvasElement;
+    //const ctx = this.chartCanvas?.nativeElement.getContext('2d');
+
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['6 months ago', '5 months ago', '4 months ago', '3 months ago', '2 months ago', '1 month ago'],
+        datasets: [
+          {
+            label: 'Price',
+            data: this.priceData,
+            borderColor: 'blue',
+            fill: false
+          }
+        ]
+      }
+    });
+  }
+
+  closeModal() {
+    this.activeModal.close();
+  }
 }
